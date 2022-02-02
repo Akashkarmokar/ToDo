76292413297e030ebd3dc99cbe7ab0c344a25cae
akashkarmokar <akashcsemu@gmail.com>
2022-02-02 23:05:14 +0600

    feat: Add Functionality of todo
    Add get,update and delete functoinallity

--- RouteHandler/todoRoute.js
+++ RouteHandler/todoRoute.js
@@ -12,13 +12,42 @@ const TodoModel = mongoose.model('ToDo', todoSchema);
  * Get all todos
  */
 route.get('/', async (req, res) => {
-    res.send('Ok');
+    try {
+        const result = await TodoModel.find({ status: 'active' }).select({ _id: 0 }).limit(2);
+        res.status(200).json({
+            result,
+            message: 'Success!',
+        });
+        console.log(result);
+    } catch (err) {
+        res.status(500).json({
+            message: 'Not Success!',
+        });
+    }
 });
 
 /**
  * Get A todo by todo id
  */
-route.get('/', async (req, res) => {});
+route.get('/:id', async (req, res) => {
+    try {
+        const result = TodoModel.find({ _id: req.params.id });
+        res.status(200).json({
+            result,
+            messae: 'Success',
+        });
+    } catch (err) {
+        if (err instanceof mongoose.Error) {
+            res.status(500).json({
+                error: 'Something is wrong with your request',
+            });
+        } else {
+            res.status(500).json({
+                error: 'server side error!',
+            });
+        }
+    }
+});
 
 /**
  * Post todo
@@ -41,14 +70,50 @@ route.post('/', async (req, res) => {
 /**
  * post multiple todo
  */
-route.post('/all', async (req, res) => {});
+route.post('/all', async (req, res) => {
+    try {
+        await TodoModel.insertMany(req.body);
+        res.status(200).json({ message: 'Data is inserted successfully!' });
+    } catch (err) {
+        res.status(500).json({ error: 'Server side error!' });
+    }
+});
 /**
  * Update todo by todo id
+ * Note : Don't use callback function with async await.
+ *
  */
-route.put('/:id', async (req, res) => {});
+route.put('/:id', async (req, res) => {
+    try {
+        const result = await TodoModel.findByIdAndUpdate(
+            { _id: req.params.id },
+            { $set: { status: 'active' } },
+            { new: true },
+        );
+        res.status(200).json({
+            message: 'Data is updated successfully!',
+        });
+        console.log(result);
+    } catch (err) {
+        res.status(500).json({
+            message: 'Server side error!',
+        });
+    }
+});
 /**
  * Delete a todo
  */
-route.delete('/:id', async (req, res) => {});
+route.delete('/:id', async (req, res) => {
+    try {
+        await TodoModel.deleteOne({ _id: req.params.id });
+        res.status(200).json({
+            message: 'succesfully deleted!',
+        });
+    } catch (error) {
+        res.status(500).json({
+            error: 'Not Deleted',
+        });
+    }
+});
 
 module.exports = route;
